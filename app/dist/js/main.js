f91aea230e4b012e5799cc4f56e3ce21b7b7041c
raaar <raffaelefacondini@raf.station>
2015-05-07 14:28:38 +0200

    swipe tested

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -186,17 +186,110 @@ var ListItem = React.createClass({displayName: "ListItem",
 		});
 	},
 
+	componentWillMount: function() {
+		React.initializeTouchEvents(true)
+	},
+
+	detect: function(e) {
+		var scope = this;
+		var item = this.refs.test.getDOMNode()
+		var link = this.refs.itemLink.getDOMNode()
+		console.log(link)
+
+
+		var  hammer = new Hammer(item, {dragLockToAxis: true, dragBlockHorizontal: true});
+
+                  var   min = 0,
+                        max = 0,
+                        moveX = 0, 
+                        startX = 0, 
+                        added = 0, 
+                        speed, 
+                        friction = 0;
+
+                  var  hammer = new Hammer(item, {dragLockToAxis: true, dragBlockHorizontal: true});
+
+                  var transformStyle = prefix() + 'Transform';
+                  var transitionStyle = prefix() + 'Transition';
+
+                  hammer.on('panstart', function() {
+                      item.style[transitionStyle] = 'none';
+                       link.setAttribute("class", "link hide");
+                  });
+
+                  hammer.on("panleft panright", function(event){
+                        moveX = startX + event.deltaX;
+                        friction = startX > min ? min/1.5 : startX;
+
+                        if (moveX < min) {
+                            friction = startX > min ? min/1.5 : startX;
+                            moveX = friction + (event.deltaX/1.3);
+                        }                          
+
+                        item.style[transformStyle] = 'translateX(' + moveX + 'px)';
+
+                        if( Math.floor((event.distance / event.target.clientWidth) * 100) > 60 ) {
+                          scope.save();
+
+                          item.setAttribute("class", "item-inner saved")
+
+                        }
+                  });
+
+                  hammer.on('panend', function(e) {
+
+                   		link.setAttribute("class", "link");
+                    
+                      speed = .2 / (Math.abs(e.velocityX) + 1);
+
+                      added += e.deltaX;
+
+                      if (added < min/2) {
+                          startX = min;
+                      }
+                      else if (added > Math.abs(min/2)) {
+                          startX = max;
+                      }
+
+                      item.style[transitionStyle] = 'all ' + speed + 's ease-in-out';
+                      item.style[transformStyle] = 'translateX(' + startX + 'px)';
+
+                      added = 0;
+
+                  });
+
+
+
+        function prefix() {
+
+            var styles = window.getComputedStyle(document.documentElement, ''),
+                pre = (Array.prototype.slice
+                    .call(styles)
+                    .join('')
+                    .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+                )[1];
+
+            return pre[0].toUpperCase() + pre.substr(1);
+
+        }
+
+
+
+	},
+
 	render: function() {
 
 		return (
 				React.createElement("li", null, 
-					React.createElement("a", {href: this.props.url, target: "_blank"}, 
-						this.props.title
-					), 
+
+    				React.createElement("div", {ref: "test", className: "item-inner", onTouchStart: this.detect}, 
+    					React.createElement("h3", null, this.props.title), 
+    					React.createElement("a", {ref: "itemLink", href: this.props.url, target: "_blank"})
+    				), 
 					
 					this.props.currentTab === 'topstories' 
 					? 
-					React.createElement("button", {onClick: this.save}, "Add")
+					React.createElement("button", {className: "hide", onClick: this.save}, "Add")
 					:
 					React.createElement("button", {onClick: this.remove}, "Remove")					 
 					
